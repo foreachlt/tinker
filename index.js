8c7b93c9aa1af91a322b16e86a6b1fad2c78a254
Kasparas Galdikas <foreachas@gmail.com>
2015-02-13 20:55:35 +0000

    Some modifications

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ require('./lib/filters')(app);
 // Routes
 require('./lib/routes')(app);
 
-// Kue
-require('./lib/kue')(app);
-
 // Start the server
 app.listen(cfg.api.PORT);
 console.info('Server started on port', cfg.api.PORT);
