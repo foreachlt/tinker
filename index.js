44c94714e89ab5afe7dc3cf089ece953a585734c
Kasparas Galdikas <foreachas@gmail.com>
2015-02-16 16:18:51 +0000

    Restructure

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-var express = require('express')
-  , app = express()
-  , logger = require('./lib/utils/logger')
-  , cfg = require('./lib/config');
+var api = require('./lib/api');
 
-// Filters
-require('./lib/filters')(app);
-
-// Routes
-require('./lib/routes')(app);
-
-// Start the server
-app.listen(cfg.api.PORT);
-logger.info('Server started on port', cfg.api.PORT);
+api();
